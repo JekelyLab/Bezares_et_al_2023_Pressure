fdeeaa19aad1b071f9dd3b8951e9398101e48c39
LBezaresCalderon <L.A.Bezares-calderon@exeter.ac.uk>
2023-04-24 13:46:30 +0100

    improved readability

--- Code/BatchBehaviour/Trackextraction.R
+++ Code/BatchBehaviour/Trackextraction.R
@@ -1,36 +1,36 @@
 #------Function to extract tracks per .res file----####
 
-Trackextraction<-function(tabnum,tables){
+Trackextraction <- function(tabnum,tables){
   totalnumTracks=0
   for(k in 1:length(tables)){
-    numTracks<-(ncol(tables[[k]])-2)/2
-    numTracksCols<-(ncol(tables[[k]])-1)
-    #XYpos<-paste(unlist(tables[[1]][,seq(2,numTracksCols,by=2)]),unlist(tables[[1]][,seq(3,numTracksCols,by=2)]),sep=",")
+    numTracks <- (ncol(tables[[k]])-2)/2
+    numTracksCols <- (ncol(tables[[k]])-1)
+    #XYpos <- paste(unlist(tables[[1]][,seq(2,numTracksCols,by=2)]),unlist(tables[[1]][,seq(3,numTracksCols,by=2)]),sep=",")
     nummerged=0
     for( z in 1: numTracks){
-      name<-as.character(paste(tabnum,"-XYPos-tr",totalnumTracks+z,sep=""))
-      MergedT<-tables[[k]] %>% unite(!!as.name(name),colnames(tables[[k]])[2+nummerged],colnames(tables[[k]])[3+nummerged],sep=",")
+      name <- as.character(paste(tabnum,"-XYPos-tr",totalnumTracks+z,sep=""))
+      MergedT <- tables[[k]] %>% unite(!!as.name(name),colnames(tables[[k]])[2+nummerged],colnames(tables[[k]])[3+nummerged],sep=",")
       if(z==1){
-        column<-tables[[k]] %>% 
+        column <- tables[[k]] %>% 
           unite(!!as.name(name),colnames(tables[[k]])[2+nummerged],colnames(tables[[k]])[3+nummerged],sep=",") %>%
           select(name)
       }
       else{
-        new<-tables[[k]] %>% 
+        new <- tables[[k]] %>% 
           unite(!!as.name(name),colnames(tables[[k]])[2+nummerged],colnames(tables[[k]])[3+nummerged],sep=",") %>%
           select(name)
-        column<-add_column(column,new)
+        column <- add_column(column,new)
       }
       nummerged=nummerged+2
     }
     if(k==1){
-      AllColumn<-column
+      AllColumn <- column
     }
     else{
-      AllColumn<-add_column(AllColumn,column)
+      AllColumn <- add_column(AllColumn,column)
     }
-    totalnumTracks<-totalnumTracks+numTracks
+    totalnumTracks <- totalnumTracks+numTracks
   }
-  AllColumn$Frame<-tables[[k]]$Frame
+  AllColumn$Frame <- tables[[k]]$Frame
   return(AllColumn)  
 }
