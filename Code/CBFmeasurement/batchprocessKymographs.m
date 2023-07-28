60e04b0ffea03c288eb77fa1d5dc513f41e5e5a3
Luis Bezares <luis.bezares@tuebingen.mpg.de>
2023-07-28 16:58:25 +0100

    writing freqs to file.

--- Code/CBFmeasurement/batchprocessKymographs.m
+++ Code/CBFmeasurement/batchprocessKymographs.m
@@ -5,11 +5,14 @@ ISTablpath = "/ebio/ag-jekely/share/Luis/Writing/Pressure_paper/publicRepo/Bezar
 FullIpath= strcat(ISTablpath, StimulusTableName);
 StimTable = readtable( FullIpath, 'ReadVariableNames',true);
 
+FOutdir = '/ebio/ag-jekely/share/Luis/Writing/Pressure_paper/publicRepo/Bezares_et_al_2023_Pressure/Data/Kymographs/CLAEHKymographs/CSV-CLAEH/FreqRidges/';
+
 %dir read
-FIndir = '/ebio/ag-jekely/share/Luis/Writing/Pressure_paper/publicRepo/Bezares_et_al_2023_Pressure/Data/Kymographs/CLEHKymographs/CSV-CLAEH/todo/';
-PatternFile = strcat(FIndir,'CLAEH*');
+FIndir = '/ebio/ag-jekely/share/Luis/Writing/Pressure_paper/publicRepo/Bezares_et_al_2023_Pressure/Data/Kymographs/CLAEHKymographs/CSV-CLAEH/Input/';
+PatternFile = strcat(FIndir,'**/CLAEH*');
 Files=dir(PatternFile);
-ListFileNames = {Files.name}
+ListFileNames = {Files.name};
+ListFileDir = {Files.folder};
 
 for m = 1:length(StimTable.Trial_ID)
     Index = find(~cellfun(@isempty,strfind(ListFileNames,StimTable.Trial_ID{m})));
@@ -17,7 +20,7 @@ for m = 1:length(StimTable.Trial_ID)
         Cellsborders = StimTable.Cell_border_start(m);
         coordCells = str2double(split(split(Cellsborders,';'),'-'))
         %Loading file
-        Image = csvread(strcat(FIndir,ListFileNames{Index}));
+        Image = csvread(strcat(ListFileDir{Index},"/",ListFileNames{Index}));
         NumSignals = 0;
         for j = 1:length(coordCells)
             BinnedI = binning(coordCells(j,1),coordCells(j,2),Image);
@@ -30,7 +33,12 @@ for m = 1:length(StimTable.Trial_ID)
             end
         end
         FreqAvg = OldFreq/NumSignals;
-        plot(FreqAvg, 'green')
+        FreqAvg = transpose(FreqAvg);
+        %writing freqs to file
+        ToName = strsplit(ListFileNames{Index},'.csv');
+        FullOutFilename = strcat(FOutdir,'Rfreq_',ToName{1},'.txt');
+        csvwrite(FullOutFilename, FreqAvg);
+        %plot(FreqAvg, 'green')
     else
         disp(strcat(StimTable.Trial_ID{m}," was not found"));
     end
@@ -39,30 +47,30 @@ end
 
 
 
-for j = 1:length(coordCells)  %loop over the cells to analyse as defined by beginning an end in the X dimension.
-    A = Image(:,coordCells(j,1):coordCells(j,2)); %subsetting the column to analyse.
-    B = mat2cell(A,repmat(blocksize(1),[1 size(A,1)/blocksize(1)]),...
-        repmat(blocksize(2),[1 size(A,2)/blocksize(2)]));  %binning the kymograph by 2, only in the X dimension.
-    B = cellfun(@(x) min(x,[],'all'),B);
-    % filename2 = "KymoRaw_190521_M1_L1_E002_minbin.csv";
-    % csvwrite(filename2,B);
-    % signal = csvread(filename2);
-
-    for i = 1:width(B)  % loop over the signal to calculate frequencies and extract the ridge.
-        [WT,freqarr,wopt]=wt(B(:,i),fs,'fmin',interval1,'fmax',interval2,'CutEdges','on',...
-                'Preprocess',preprocess,'Wavelet',w_type,'f0',f0);
-        tfsupp = ecurve(WT,freqarr,wopt);
-        if (i == 1) && (j == 1)
-            tfsuppOld = tfsupp;
-        else 
-            tfsuppOld = tfsupp + tfsuppOld;
-        end
-        totalSignals = totalSignals + 1;
-        strcat(num2str(j),'\n',num2str(i));
-    end
-end
- tfsuppAvg = tfsuppOld/totalSignals;
- %tfsuppAvg = tfsuppOld/19;
- %plot(tfsuppAvg(1,:),'red')
- [iamp,iphi,ifreq] = rectfr(tfsuppAvg,WT,freqarr,wopt,'ridge');
- plot(ifreq, 'red')
+% for j = 1:length(coordCells)  %loop over the cells to analyse as defined by beginning an end in the X dimension.
+%     A = Image(:,coordCells(j,1):coordCells(j,2)); %subsetting the column to analyse.
+%     B = mat2cell(A,repmat(blocksize(1),[1 size(A,1)/blocksize(1)]),...
+%         repmat(blocksize(2),[1 size(A,2)/blocksize(2)]));  %binning the kymograph by 2, only in the X dimension.
+%     B = cellfun(@(x) min(x,[],'all'),B);
+%     % filename2 = "KymoRaw_190521_M1_L1_E002_minbin.csv";
+%     % csvwrite(filename2,B);
+%     % signal = csvread(filename2);
+% 
+%     for i = 1:width(B)  % loop over the signal to calculate frequencies and extract the ridge.
+%         [WT,freqarr,wopt]=wt(B(:,i),fs,'fmin',interval1,'fmax',interval2,'CutEdges','on',...
+%                 'Preprocess',preprocess,'Wavelet',w_type,'f0',f0);
+%         tfsupp = ecurve(WT,freqarr,wopt);
+%         if (i == 1) && (j == 1)
+%             tfsuppOld = tfsupp;
+%         else 
+%             tfsuppOld = tfsupp + tfsuppOld;
+%         end
+%         totalSignals = totalSignals + 1;
+%         strcat(num2str(j),'\n',num2str(i));
+%     end
+% end
+%  tfsuppAvg = tfsuppOld/totalSignals;
+%  %tfsuppAvg = tfsuppOld/19;
+%  %plot(tfsuppAvg(1,:),'red')
+%  [iamp,iphi,ifreq] = rectfr(tfsuppAvg,WT,freqarr,wopt,'ridge');
+%  plot(ifreq, 'red')
